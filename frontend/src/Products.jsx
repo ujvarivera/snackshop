dcc6da19f367f20aa35b0dca920695c54cdcdacc
ujvarivera <ujvariveronika0320@gmail.com>
2025-07-22 19:41:01 +0200

    listing products on frontend

--- frontend/src/Products.jsx
+++ frontend/src/Products.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 
 const Products = () => {
     const { user } = useAuth();
+    const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/products`, {
+            withCredentials: true,
+        })
+        .then(res => setProducts(res.data))
+        .catch(err => console.error(err));
+    }, []);
 
     return (
-        <>
-        <h2>Welcome, {user?.name}</h2>
-        {user?.isAdmin && <p>You are an admin.</p>}
-        Products
-        </>
+        <div>
+            <h2>Welcome, {user?.name}</h2>
+            {user?.isAdmin && <p>You are an admin.</p>}
+            <ul>
+                {products.map(p => (
+                <li key={p.id}>{p.name} - {p.price} {p.currency}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
